447b5bbfe568a2d8000f2b78c425a65f5395678d
Wojciech Sękowski <wsekowski@poczta.onet.pl>
2022-01-13 20:56:58 +0100

    some changes

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -4,47 +4,105 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
-
 from typing import Any, Text, Dict, List
 import json
 from pathlib import Path
 
 from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet,EventType, Form
 from rasa_sdk.executor import CollectingDispatcher
 
-
-class ActionHelloWorld(Action):
-
+class ActionOrderId(Action):
     def name(self) -> Text:
-        return "action_hello_world"
+        return "action_order_id"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
+    async def run(self, dispatcher: CollectingDispatcher,tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        order_id=tracker.sender_id
+        dispatcher.utter_message("Your order number is {}".format(order_id))
 
-        dispatcher.utter_message(text="Hello World!")
+class ActionAskOrder(Action):
+    def name(self) -> Text:
+        return "action_ask_order"
+
+    def run(self, dispatcher: CollectingDispatcher,tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        sentence=''
+        with open("data/menu.json") as f:
+        
+            data=json.load(f)
+            sentence+="Menu:\n"
+            for row in data['items']:
+                sentence+="Meal " + str(row['name'])+" -> price: "+str(row['price'])+ "$ -> preparation time: "+ str(row['preparation_time']*60)+" min\n"
+
+        dispatcher.utter_message(sentence)
+        return []
+
+class ActionOpeningHours(Action):
+    def name(self) -> Text:
+        return "action_opening_hours"
+
+    def run(self, dispatcher: CollectingDispatcher,tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        sentence=''
+        with open("data/opening_hours.json") as f:
+            data2=json.load(f)
+
+            sentence+="Restaurant opening hours are:\n"
+            for row in data2['items']:
+                if(row!="Sunday"):
+                    sentence+=row+ " "+str(data2['items'][row]['open'])+" - "+ str(data2['items'][row]['close'])+'\n'
+            sentence+="It is closed on Sunday"
+
+        dispatcher.utter_message(sentence)
+        return []
+
+
+class ActionFormInfo(Action):
+    def name(self)-> Text:
+        return "form info"
+
+    @staticmethod
+    def required_slots(tracker: Tracker):
+        return ["food","size", "quantity", "number","name"]
+
+    def submit(self, dispatcher: CollectingDispatcher,tracker: Tracker,
+            domain: Dict[Text, Any]):
+        dispatcher.utter_message(template="utter_thanks", name=tracker.get_slot('name'), 
+        order=tracker.sender_id)
+        dispatcher.utter_message(template="utter_food", quantity=tracker.get_slot('quantity'))
 
         return []
 
-class ActionCheckExistence(Action):
-    # data=Path("data/opening_hours.json").read_text.split("\n")
-    f=open('data/menu.json')
-    data=json.load(f)
+    def slot_mappings(self):
+        return {
+            "name": [self.from_entity(entity="name", intent='provide_name'), self.from_text()],
+            "food": [self.from_entity(entity="food", intent='food'), self.from_text()],
+            "quantity": [self.from_entity(entity="quantity", intent='quantity'), self.from_text()],
+            "number": [self.from_entity(entity="number", intent='provide_number'), self.from_text()]
+        }
 
-    def name(self) -> Text:
-        return "action_check_existence"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        for blob in tracker.latest_message['entities']:
-            print(tracker.latest_message)
-            if blob['entity']=='pokemon_name':
-                name=blob['value']
-                if name in self.data:
-                    dispatcher.utter_message(text="Hello World!")
-                else:
-                    dispatcher.utter_message(text="else Hello World!")
 
-        return []  
+# class ActionCheckExistence(Action):
+
+#     def name(self) -> Text:
+#         return "action_get_food_from_user"
+
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+#         for blob in tracker.latest_message['entities']:
+            # print(tracker.latest_message)
+#             if blob['entity']=='food':
+#                 name=blob['value']
+#                 if name in self.data:
+#                     dispatcher.utter_message(text="Hello World!")
+#                 else:
+#                     dispatcher.utter_message(text="else Hello World!")
+
+#         return []  
--- chatbot/data/stories.md
+++ chatbot/data/stories.md
@@ -3,3 +3,41 @@
     - utter_greet
 * mood_great
     - utter_happy
+* affirm
+    - utter_goodbye
+
+## search restaurant happy path
+
+
+* search_resturant
+
+  - action_search_resturant
+
+  - utter_did_that_help
+
+
+
+## search restaurant sad path
+
+* greet
+  - utter_greet
+
+* search_resturant
+
+  - action_search_resturant
+
+  - utter_did_that_help
+
+* deny
+
+  - utter_default
+
+* search_resturant
+
+  - action_search_resturant
+
+  - utter_did_that_help
+
+* affirm
+
+  - utter_goodbye
